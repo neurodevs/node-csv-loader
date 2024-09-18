d4b4b4bcc81d404eb3d54664935a587f16333568
eric-yates <hello@ericthecurious.com>
2024-09-18 01:07:16 -0600

    patch: delete extracted tests

--- src/__tests__/behavioral/CsvLoader.test.ts
+++ src/__tests__/behavioral/CsvLoader.test.ts
@@ -1,18 +1,11 @@
 import fs from 'fs'
-import AbstractSpruceTest, {
-    test,
-    assert,
-    errorAssert,
-    generateId,
-} from '@sprucelabs/test-utils'
+import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
 import { FileLoaderOptions } from '@neurodevs/node-file-loader'
 import csvParser from 'csv-parser'
 import CsvLoaderImpl, { CsvRow } from '../../CsvLoader'
 import SpyCsvLoader from '../testDoubles/SpyCsvLoader'
 
 export default class CsvLoaderTest extends AbstractSpruceTest {
-    private static invalidExtensionPath: string
-    private static doesNotExistPath: string
     private static actualPath: string
     private static expectedData: CsvRow[]
 
@@ -23,8 +16,6 @@ export default class CsvLoaderTest extends AbstractSpruceTest {
 
         CsvLoaderImpl.Class = SpyCsvLoader
 
-        this.invalidExtensionPath = generateId()
-        this.doesNotExistPath = `${generateId()}.csv`
         this.actualPath = 'src/__tests__/testData/test.csv'
         this.expectedData = await this.loadCsv(this.actualPath)
 
@@ -35,64 +26,6 @@ export default class CsvLoaderTest extends AbstractSpruceTest {
     @test()
     protected static async canCreateCsvLoader() {
         assert.isTruthy(this.loader)
-    }
-
-    @test()
-    protected static async throwsWithMissingRequiredOptions() {
-        const err = await assert.doesThrowAsync(
-            // @ts-ignore
-            async () => await this.load()
-        )
-
-        errorAssert.assertError(err, 'MISSING_PARAMETERS', {
-            parameters: ['path'],
-        })
-    }
-
-    @test()
-    protected static async throwsIfPathDoesNotExist() {
-        const err = await assert.doesThrowAsync(
-            async () => await this.loader.load(this.doesNotExistPath)
-        )
-
-        errorAssert.assertError(err, 'FILE_NOT_FOUND', {
-            path: this.doesNotExistPath,
-        })
-    }
-
-    @test()
-    protected static async throwsWithInvalidFileExtension() {
-        const err = await assert.doesThrowAsync(
-            async () => await this.loader.load(this.invalidExtensionPath)
-        )
-
-        errorAssert.assertError(err, 'INVALID_FILE_EXTENSION', {
-            expected: '.csv',
-            path: this.invalidExtensionPath,
-        })
-    }
-
-    @test()
-    protected static async throwsIfLoadFails() {
-        const fakeError = 'Unhandled error'
-        this.loader.setThrowOnLoadFile(fakeError)
-
-        const err = await assert.doesThrowAsync(
-            async () => await this.load(this.actualPath)
-        )
-
-        errorAssert.assertError(err, 'FILE_LOAD_FAILED', {
-            path: this.actualPath,
-            originalError: fakeError,
-        })
-    }
-
-    @test()
-    protected static async canTurnOffValidation() {
-        const loader = this.Loader({ shouldValidatePath: false })
-
-        await loader.load(this.actualPath)
-        assert.isEqual(loader.numCallsToValidatePath, 0)
     }
 
     @test()
